01f5db8f6c73708cf7f8db22805527e58d95f011
adlarajasri <129931618+adlarajasri@users.noreply.github.com>
2023-04-28 22:07:22 +0530

    Add files via upload

---
+++ Day13/MulticastDelegate/MulticastDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulticastDelegate
+{
+    delegate int CountIt(int end);
+     class Program
+    {
+        public delegate int Sum(int x, int y);
+        static void Main(string[] args)
+        {
+            int result;
+            Sum s = delegate (int n, int m)
+            {
+                return n + m;
+            };
+            result = s(10, 20);
+            Console.WriteLine(result);
+        }
+    }
+}
